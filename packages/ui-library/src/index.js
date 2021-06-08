9a48769d4d8f65002e6d39ade6d125cc53da1fbc
Jorn Luiten <contact@frontrain.net>
2021-06-08 15:14:20 +0200

    working on tree shaking

--- packages/ui-library/src/index.js
+++ packages/ui-library/src/index.js
@@ -8,22 +8,22 @@ export { default as Alert } from './components/Alert'
 export { default as Button } from './components/Button'
 export { default as CGLogo } from './components/CGLogo'
 export { default as Collapsible } from './components/Collapsible'
-export {
-  Drawer,
-  withDrawerStack,
-  useDrawerStack,
-  StackedDrawer,
-  MockDrawerStack,
-} from './components/Drawer'
-export { default as GlobalStyles } from './components/GlobalStyles'
+// export {
+//   Drawer,
+//   withDrawerStack,
+//   useDrawerStack,
+//   StackedDrawer,
+//   MockDrawerStack,
+// } from './components/Drawer'
+// export { default as GlobalStyles } from './components/GlobalStyles'
 export { default as GitLabLogo } from './components/GitLabLogo'
 export { default as NLXLogo } from './components/NLXLogo'
-export {
-  default as PrimaryNavigation,
-  mobileNavigationHeight,
-} from './components/PrimaryNavigation'
-export { default as Icon } from './components/Icon'
-export { default as IconFlippingChevron } from './components/IconFlippingChevron'
+// export {
+//   default as PrimaryNavigation,
+//   mobileNavigationHeight,
+// } from './components/PrimaryNavigation'
+// export { default as Icon } from './components/Icon'
+// export { default as IconFlippingChevron } from './components/IconFlippingChevron'
 export { default as Link } from './components/Link'
 export { default as Spinner } from './components/Spinner'
-export { ToasterProvider, ToasterContext, Toast } from './components/Toaster'
+// export { ToasterProvider, ToasterContext, Toast } from './components/Toaster'
